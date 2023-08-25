a5bd21af92759e275f6baef8b1b909fd7f124531
YBKR <121393040+YBKR@users.noreply.github.com>
2023-08-25 14:52:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let buttons = document.getElementByIdTagName('button');
+let result = document.getElementById('result');
+
+for (i=0;i<buttons.length;i++)
+{
+    buttons[i].onclick = function()
+    {
+        let input = this.innerText;
+        updateDisplay(input);
+    }
+}
+
+function updateDisplay(input)
+{
+    let currentDisplay =result.innerText;
+;
+if(currentDisplay=="0")
+{
+    if(input!="C" && input!="DEL" && input!="=")
+    {
+        result.innerText= "";
+        result.innerText = input;
+
+    }
+}
+else {
+    if(input=="DEL")
+    {
+        result.innerText=currentDisplay.substring(0,currentDisplay.length-1);
+        if(result.innerText="")
+        {
+            result.innerText="0";
+        }
+    }
+
+    if(input=="C")
+    {
+        result.innerText="0"; 
+    }
+
+    if(input!="C" && input!="DEL" && input!="=")
+    {
+        result.innerText += input;
+    }
+
+    if(input=="=")
+    {
+        let display=result.innerText;
+        result.innerText=eval(display);
+
+    }
+ }
+}
